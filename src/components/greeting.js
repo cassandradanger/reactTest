0cd2e1bbfc902e3c8ffd2f14688b178611ea5896
Casie Lynch <casie@primeacademy.io>
2016-09-28 15:03:25 -0500

    updated greeting.js file

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -4,44 +4,26 @@
 import React from 'react'
 
 export default class Greeting extends React.Component {
-    render(){
-        return(
-            <h1 className="greeting">Hello, {this.props.name}!</h1>
+    constructor (props) {
+        super(props)
+        this.state = {
+            name: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange (event) {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    render () {
+        return (
+            <div>
+                <h1 className='greeting'>Hello, {this.state.name || this.props.name}!</h1>
+                <input type='text' placeholder='Enter your name' onChange={this.handleChange} />
+            </div>
         )
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//     constructor (props) {
-//         super(props)
-//         this.state = {
-//             name: ''
-//         }
-//         this.handleChange = this.handleChange.bind(this)
-//     }
-//
-//     handleChange (event) {
-//         this.setState({
-//             name: event.target.value
-//         })
-//     }
-//
-//     render () {
-//         return (
-//             <div>
-//                 <h1 className='greeting'>Hello, {this.state.name || this.props.name}!</h1>
-//                 <input type='text' placeholder='Enter your name' onChange={this.handleChange} />
-//             </div>
-//         )
-//     }
-// }
